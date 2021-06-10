2927c51865eb3357c036bbb646a7f1d0cf651f3e
Alex Marriott <marriottaog@gmail.com>
2021-06-10 12:12:53 +0100

    WIP lists and conditionals

--- 03-lists-and-conditionals/src/components/Expenses/Expenses.js
+++ 03-lists-and-conditionals/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2022');
@@ -18,14 +18,18 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
       <ExpensesFilter onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))};
+      <ExpensesList filteredExpenses={filteredExpenses} />
+{/*        {filteredExpenses.length === 0 && <p>No expenses found</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          )
+        )};*/}
       </Card>
     </div>
   );
---
+++ 03-lists-and-conditionals/src/components/Expenses/ExpensesList.js
@@ -0,0 +1,22 @@
+import './ExpensesList.css'
+import ExpenseItem from './ExpenseItem';
+
+
+const ExpensesList = (props) => {
+  let expensesContent = <p>No expenses found</p>
+
+  if (props.filteredExpenses.length > 0) {
+    expensesContent = props.filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
+  return {expensesContent};
+}
+
+export default ExpensesList;
